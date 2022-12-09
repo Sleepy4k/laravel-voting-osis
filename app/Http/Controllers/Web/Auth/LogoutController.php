c347283939830595f9898f3c4aa35812a988e0e0
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-10 02:39:00 +0700

    feat: some controller
    add login and logout controller

---
+++ app/Http/Controllers/Web/Auth/LogoutController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Web\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\WebController;
+use App\Services\Web\Auth\LogoutService;
+
+class LogoutController extends WebController
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, LogoutService $service)
+    {
+        try {
+            $service->store($request);
+    
+            return to_route('landing.index');
+        } catch (\Throwable $th) {
+            return $this->redirectError($th);
+        }
+    }
+}
