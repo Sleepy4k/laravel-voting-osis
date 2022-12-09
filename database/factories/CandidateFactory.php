43e965acbee9b140cb15a0e96357c214a7e7413e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-10 00:04:32 +0700

    feat: some factory
    add candidate and voting factory

---
+++ database/factories/CandidateFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Candidate>
+ */
+class CandidateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'chairman' => fake()->unique()->name(),
+            'vice_chairman' => fake()->unique()->name(),
+            'image' => 'candidate.jpg',
+            'vision' => fake()->text(150),
+            'mission' => fake()->sentence(255)
+        ];
+    }
+}
