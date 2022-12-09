5a06504eaaacabfa70e052fcdec8e63bb083b582
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-10 02:40:03 +0700

    chore: base service
    remove unused interface

--- app/Services/Service.php
+++ app/Services/Service.php
@@ -25,16 +25,6 @@ class Service
     protected $auditInterface;
 
     /**
-     * @var companyInterface
-     */
-    protected $companyInterface;
-
-    /**
-     * @var concertInterface
-     */
-    protected $concertInterface;
-
-    /**
      * @var languageInterface
      */
     protected $languageInterface;
@@ -50,43 +40,29 @@ class Service
     protected $applicationInterface;
 
     /**
-     * @var transactionInterface
-     */
-    protected $transactionInterface;
-
-    /**
      * Model contract constructor.
      * 
      * @param App\Contracts\Models\UserInterface $userInterface
      * @param App\Contracts\Models\RoleInterface $roleInterface
      * @param App\Contracts\Models\AuditInterface $auditInterface
-     * @param App\Contracts\Models\CompanyInterface $companyInterface
-     * @param App\Contracts\Models\ConcertInterface $concertInterface
      * @param App\Contracts\Models\LanguageInterface $languageInterface
      * @param App\Contracts\Models\PermissionInterface $permissionInterface
      * @param App\Contracts\Models\ApplicationInterface $applicationInterface
-     * @param App\Contracts\Models\TransactionInterface $transactionInterface
      */
     public function __construct(
         Models\UserInterface $userInterface,
         Models\RoleInterface $roleInterface,
         Models\AuditInterface $auditInterface,
-        Models\CompanyInterface $companyInterface,
-        Models\ConcertInterface $concertInterface,
         Models\LanguageInterface $languageInterface,
         Models\PermissionInterface $permissionInterface,
-        Models\ApplicationInterface $applicationInterface,
-        Models\TransactionInterface $transactionInterface,
+        Models\ApplicationInterface $applicationInterface
     )
     {
         $this->userInterface = $userInterface;
         $this->roleInterface = $roleInterface;
         $this->auditInterface = $auditInterface;
-        $this->companyInterface = $companyInterface;
-        $this->concertInterface = $concertInterface;
         $this->languageInterface = $languageInterface;
         $this->permissionInterface = $permissionInterface;
         $this->applicationInterface = $applicationInterface;
-        $this->transactionInterface = $transactionInterface;
     }
 }
