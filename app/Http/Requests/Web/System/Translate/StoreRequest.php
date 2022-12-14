a912e790753e229fa0473b160c6a9e2dc9775f3f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-14 19:29:53 +0700

    feat: translate form request
    add translate form request

---
+++ app/Http/Requests/Web/System/Translate/StoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Web\System\Translate;
+
+use App\Http\Requests\WebRequest;
+
+class StoreRequest extends WebRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'group' => ['required','string','max:255'],
+            'key' => ['required','string','max:255','unique:language_lines,key'],
+            'lang_id' => ['required','string','max:255'],
+            'lang_en' => ['required','string','max:255']
+        ];
+    }
+}
