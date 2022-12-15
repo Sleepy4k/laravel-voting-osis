3b48eba31cd33deaa1d455a2f327813757d55832
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-16 00:28:16 +0700

    feat: some form request
    add menu and page

---
+++ app/Http/Requests/Web/System/Menu/UpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Web\System\Menu;
+
+use App\Http\Requests\WebRequest;
+
+class UpdateRequest extends WebRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $id = basename(request()->path());
+
+        return [
+            'name' => ['required','string','max:255','unique:menus,name,'.$id],
+            'label' => ['required','string','max:255'],
+            'icon' => ['required','string','max:255'],
+            'ordering' => ['required','string','max:255'],
+            'role' => ['required','string','max:255']
+        ];
+    }
+}
