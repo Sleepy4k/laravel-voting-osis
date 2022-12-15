79a3fa13d34f7a4a01f086a82fea5737d26dde2f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-16 00:29:20 +0700

    feat: some config
    add menu and page

---
+++ config/page.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+    'admin' => [
+        [
+           'name' => 'dashboard',
+           'label' => 'sidebar.admin.page.dashboard',
+           'menu_id' => 1,
+           'route' => 'admin.dashboard.index',
+           'icon' => 'bi bi-grid-fill'
+        ],
+        [
+           'name' => 'candidate',
+           'label' => 'sidebar.admin.page.candidate',
+           'menu_id' => 1,
+           'route' => 'admin.candidate.index',
+           'icon' => 'bi bi-people-fill'
+        ],
+        [
+           'name' => 'user',
+           'label' => 'sidebar.admin.page.user',
+           'menu_id' => 1,
+           'route' => 'admin.user.index',
+           'icon' => 'bi bi-people-fill'
+        ],
+        [
+            'name' => 'translate',
+            'label' => 'sidebar.admin.page.translate',
+            'menu_id' => 2,
+            'route' => 'admin.system.translate.index',
+            'icon' => 'bi bi-grid-fill'
+        ],
+        [
+            'name' => 'menu',
+            'label' => 'sidebar.admin.page.menu',
+            'menu_id' => 2,
+            'route' => 'admin.system.menu.index',
+            'icon' => 'bi bi-grid-fill'
+        ],
+        [
+            'name' => 'page',
+            'label' => 'sidebar.admin.page.page',
+            'menu_id' => 2,
+            'route' => 'admin.system.page.index',
+            'icon' => 'bi bi-grid-fill'
+        ],
+        [
+            'name' => 'auth',
+            'label' => 'sidebar.admin.page.auth',
+            'menu_id' => 3,
+            'route' => 'admin.audit.auth.index',
+            'icon' => 'bi bi-grid-fill'
+        ],
+        [
+            'name' => 'model',
+            'label' => 'sidebar.admin.page.model',
+            'menu_id' => 3,
+            'route' => 'admin.audit.model.index',
+            'icon' => 'bi bi-grid-fill'
+        ],
+        [
+            'name' => 'query',
+            'label' => 'sidebar.admin.page.query',
+            'menu_id' => 3,
+            'route' => 'admin.audit.query.index',
+            'icon' => 'bi bi-grid-fill'
+        ],
+        [
+            'name' => 'system',
+            'label' => 'sidebar.admin.page.system',
+            'menu_id' => 3,
+            'route' => 'admin.audit.system.index',
+            'icon' => 'bi bi-grid-fill'
+        ]
+    ]
+];
