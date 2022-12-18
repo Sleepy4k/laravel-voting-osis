9a019b77621ce208ce4373dbfa47e0a597d1505a
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-18 13:04:43 +0700

    chore: some config
    add sidebar menu and page

--- config/menu.php
+++ config/menu.php
@@ -2,23 +2,29 @@
 
 return [
     'admin' => [
-        [
+         [
            'name' => 'main',
            'label' => 'sidebar.admin.menu.main',
            'ordering' => 1,
            'role' => 'admin|superadmin'
-        ],
-        [
-           'name' => 'system',
-           'label' => 'sidebar.admin.menu.system',
+         ],
+         [
+           'name' => 'admin',
+           'label' => 'sidebar.admin.menu.admin',
            'ordering' => 2,
            'role' => 'superadmin'
-        ],
-        [
-           'name' => 'audit',
-           'label' => 'sidebar.admin.menu.audit',
+         ],
+         [
+           'name' => 'system',
+           'label' => 'sidebar.admin.menu.system',
            'ordering' => 3,
            'role' => 'superadmin'
-        ]
+         ],
+         [
+           'name' => 'audit',
+           'label' => 'sidebar.admin.menu.audit',
+           'ordering' => 4,
+           'role' => 'superadmin'
+         ]
     ]
 ];
--- config/page.php
+++ config/page.php
@@ -24,58 +24,72 @@ return [
            'icon' => 'bi bi-people-fill'
         ],
         [
+           'name' => 'role',
+           'label' => 'sidebar.admin.page.role',
+           'menu_id' => 2,
+           'route' => 'admin.role.index',
+           'icon' => 'bi bi-grid-fill'
+        ],
+        [
+           'name' => 'permission',
+           'label' => 'sidebar.admin.page.permission',
+           'menu_id' => 2,
+           'route' => 'admin.permission.index',
+           'icon' => 'bi bi-grid-fill'
+        ],
+        [
             'name' => 'menu',
             'label' => 'sidebar.admin.page.menu',
-            'menu_id' => 2,
+            'menu_id' => 3,
             'route' => 'admin.system.menu.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'page',
             'label' => 'sidebar.admin.page.page',
-            'menu_id' => 2,
+            'menu_id' => 3,
             'route' => 'admin.system.page.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'translate',
             'label' => 'sidebar.admin.page.translate',
-            'menu_id' => 2,
+            'menu_id' => 3,
             'route' => 'admin.system.translate.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'application',
             'label' => 'sidebar.admin.page.application',
-            'menu_id' => 2,
+            'menu_id' => 3,
             'route' => 'admin.system.application.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'auth',
             'label' => 'sidebar.admin.page.auth',
-            'menu_id' => 3,
+            'menu_id' => 4,
             'route' => 'admin.audit.auth.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'model',
             'label' => 'sidebar.admin.page.model',
-            'menu_id' => 3,
+            'menu_id' => 4,
             'route' => 'admin.audit.model.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'query',
             'label' => 'sidebar.admin.page.query',
-            'menu_id' => 3,
+            'menu_id' => 4,
             'route' => 'admin.audit.query.index',
             'icon' => 'bi bi-grid-fill'
         ],
         [
             'name' => 'system',
             'label' => 'sidebar.admin.page.system',
-            'menu_id' => 3,
+            'menu_id' => 4,
             'route' => 'admin.audit.system.index',
             'icon' => 'bi bi-grid-fill'
         ]
